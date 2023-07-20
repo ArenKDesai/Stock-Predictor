1072f4189f0ebe17a9cb30088bdabfed6c7097dd
Aren Desai <akdesai@wisc.edu>
2023-07-20 17:10:16 -0500

    finished simple model

--- Stock_Predictor.py
+++ Stock_Predictor.py
@@ -6,6 +6,7 @@ import sklearn.model_selection
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
+from keras.models import load_model
 
 os.chdir("C://Users//arenk//Documents//stock_predictor")
 
@@ -21,3 +22,24 @@ for i in range(60, 1258):
     y_train.append(scaled_training_set[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+regressor = load_model('Stock_Predictor.keras')
+
+actual_stock_price = testing_set.iloc[0:20, 2:3].values
+dataset_total = pd.concat((dataset['Open'], testing_set['Open']), axis=0)
+inputs = dataset_total[len(dataset_total)-len(testing_set)-60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = scaler.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = scaler.inverse_transform(predicted_stock_price)
+plt.plot(actual_stock_price, color='red', label='Actual Stock Price') # Something's up with this line
+plt.plot(predicted_stock_price, color='blue', label='Predicted Stock Price')
+plt.title('Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
