245b519ddcc19ded80b9f8453abc73857ab19c69
Aren Desai <akdesai@wisc.edu>
2023-07-29 19:42:12 -0500

    getting javafx to work

---
+++ App.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane root = new StackPane();
+        primaryStage.setTitle("Stock Prediction Launcher");
+        primaryStage.setScene(new Scene(root, 80, 60));
+        Label stock_name = new Label("Stock Name");
+         Button submitButton = new Button("Submit");
+        TextField stock_name_input = new TextField();
+        VBox vbox = new VBox(stock_name, stock_name_input, submitButton);
+
+        submitButton.setOnAction(e -> {
+            String inputText = stock_name_input.getText();
+
+        });
+
+        root.getChildren().add(vbox);
+        primaryStage.show();
+    }
+}
--- kaggle_prediction.py
+++ kaggle_prediction.py
@@ -1,96 +1,76 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
-
-# Reads stock data from Yahoo
-from pandas_datareader.data import DataReader
 import yfinance as yf
 from pandas_datareader import data as pdr
-
-# Model building
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+import sys
 
-yf.pdr_override()
+def predict(stock_name):
+    df = pdr.get_data_yahoo(stock_name, start = '2012-01-01', end=datetime.now())
+    data = df.filter(["Close"])
+    dataset = data.values
+    training_data_len = int(np.ceil(len(dataset) * 0.95))
 
-# Tech stocks template
-tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'IBM', 'RSKD']
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(dataset)
 
-end = datetime.now()
-start = datetime(end.year - 1, end.month, end.day)
+    train_data = scaled_data[0:int(training_data_len),:]
+    x_train = []
+    y_train = []
 
-for stock in tech_list:
-    globals()[stock] = yf.download(stock, start, end)
+    for i in range(60, len(train_data)):
+        x_train.append(train_data[i-60:i, 0])
+        y_train.append(train_data[i,0])
+        if i<=61:
+            print(x_train)
+            print(y_train)
+            print()
 
-company_list = [AAPL, GOOG, MSFT, AMZN, IBM, RSKD] # There is no error here, these are pandas dataframes
-company_name = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON", "IBM", "RISKIFIED"]
+    x_train, y_train = np.array(x_train), np.array(y_train)
 
-for company, com_name in zip(company_list, company_name):
-    company["company_name"] = com_name
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-df = pd.concat(company_list, axis = 0)
+    model = Sequential()
+    model.add(LSTM(128, return_sequences=True, input_shape = (x_train.shape[1], 1)))
+    model.add(LSTM(64, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
 
-# Beginning the prediction
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, batch_size = 1, epochs = 1)
 
-df = pdr.get_data_yahoo('RSKD', start = '2012-01-01', end=datetime.now())
-data = df.filter(["Close"])
-dataset = data.values
-training_data_len = int(np.ceil(len(dataset) * 0.95))
+    test_data = scaled_data[training_data_len - 60:,:]
+    x_test=[]
+    y_test=dataset[training_data_len:,:]
+    for i in range(60, len(test_data)):
+        x_test.append(test_data[i-60:i, 0])
 
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(dataset)
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-train_data = scaled_data[0:int(training_data_len),:]
-x_train = []
-y_train = []
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
 
-for i in range(60, len(train_data)):
-    x_train.append(train_data[i-60:i, 0])
-    y_train.append(train_data[i,0])
-    if i<=61:
-        print(x_train)
-        print(y_train)
-        print()
+    rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
 
-x_train, y_train = np.array(x_train), np.array(y_train)
+    train = data[:training_data_len]
+    valid = data[training_data_len:]
+    valid["Predictions"] = predictions
+    plt.figure(figsize=(16,6))
+    plt.title('Model')
+    plt.xlabel('Date', fontsize=18)
+    plt.ylabel('Close Price USD ($)', fontsize=18)
+    plt.plot(train['Close'])
+    plt.plot(valid[['Close', 'Predictions']])
+    # plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+    plt.show()
 
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-model = Sequential()
-model.add(LSTM(128, return_sequences=True, input_shape = (x_train.shape[1], 1)))
-model.add(LSTM(64, return_sequences=False))
-model.add(Dense(25))
-model.add(Dense(1))
-
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, batch_size = 1, epochs = 1)
-
-test_data = scaled_data[training_data_len - 60:,:]
-x_test=[]
-y_test=dataset[training_data_len:,:]
-for i in range(60, len(test_data)):
-    x_test.append(test_data[i-60:i, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
-
-rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
-
-train = data[:training_data_len]
-valid = data[training_data_len:]
-valid["Predictions"] = predictions
-plt.figure(figsize=(16,6))
-plt.title('Model')
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Close Price USD ($)', fontsize=18)
-plt.plot(train['Close'])
-plt.plot(valid[['Close', 'Predictions']])
-# plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
-plt.show()
+if __name__ == '__main__':
+    yf.pdr_override()
+    stock_name = sys.argv[1]
+    predict(stock_name)
